b00d24d9fa536845f9be7cd047aa02888841e581
utkarshk-iitr <utkarsh_k@cs.iitr.ac.in>
2025-12-04 15:17:08 +0530

    resumption done using standard method

--- Utk/wolf_setup.sh
+++ Utk/wolf_setup.sh
@@ -3,7 +3,7 @@
 git clone https://github.com/wolfSSL/wolfssl.git
 cd wolfssl
 ./autogen.sh
-./configure --enable-kyber --enable-dilithium --enable-shared --enable-aesgcm --enable-dtls --enable-dtls13 --enable-tls --enable-tls13 --enable-dtls-frag-ch --enable-keygen --enable-certgen --enable-certreq --enable-opensslall --enable-all --enable-asn
+./configure --enable-psk --enable-session-ticket --enable-session-export --enable-kyber --enable-dilithium --enable-shared --enable-aesgcm --enable-dtls --enable-dtls13 --enable-tls --enable-tls13 --enable-dtls-frag-ch --enable-keygen --enable-certgen --enable-certreq --enable-opensslall --enable-all --enable-asn
 
 make -j$(nproc)
 sudo make install
